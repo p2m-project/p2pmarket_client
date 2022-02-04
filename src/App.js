9f9e006a3607d41c6cc3fbcf2173204c21b074a5
Timothy Karani <kartimothy@gmail.com>
2022-02-04 21:19:27 +0300

    Installed everything needed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import SignInScreen from "./pages/auth/signin";
 import LandingScreen from "./pages/landing";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/signIn" element={<SignInScreen />} />
-          <Route exact path="/" element={<LandingScreen />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route exact path="/signIn" element={<SignInScreen />} />
+        <Route exact path="/" element={<LandingScreen />} />
+      </Routes>
     </div>
   );
 }
